5100de3e8b70e08f76c29363b46c2e3f469f88d1
Vuk Urosevic <vuk23urosevic@gmail.com>
2025-08-11 11:39:05 +0200

    frontend base, started over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,35 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import { lazy, Suspense, useEffect, useState } from "react";
-import { Spinner } from "react-bootstrap";
-import { isAuthenticated } from "./auth";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// Lazy-loading is a technique that allows components to be loaded only when needed, e.g. when a route is hit
-// This means components are loaded asynchronously, which means they load in the background
-const HomePage = lazy(() => import("./pages/HomePage"));
-const AllSubjects = lazy(() => import("./pages/AllSubjects"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
+function App() {
+  const [count, setCount] = useState(0)
 
-// This is a private route that checks if the user is logged in so
-// element is the component we want to show if the user is logged in
-// if false then we redirect the user to the login page
-interface PrivateRouteProps {
-    element: JSX.Element;
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
-    return isAuthenticated() ? element : <Navigate to="/login" />;
-};
-
-function App(): JSX.Element {
-    const [currentDate, setCurrentDate] = useState<string>('');
-
-    useEffect(() => {
-        const date = new Date(); // padStart adds zeros in front of number until length is 2
-        const formattedDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`;
-        setCurrentDate(formattedDate);
-    }, []);
-
-    return (
-        <>
-            <div className="App">
-                <Router>
-                    <div className="app-container">
-                        <NavigationBar />
-                        <main className="main-content">
-                            {/* Suspense is a component that allows something to be executed while the page is loading.
-                             For us it's the Spinner component (loading circle) */}
-                            <Suspense fallback={<Spinner animation="border" role="status">
-                                <span className="visually-hidden">Loading...</span>
-                            </Spinner>}>
-                                <Routes>
-                                    {/* When a route is hit, the component (element) that corresponds to the route is displayed */}
-                                    <Route path="/" element={<HomePage />} />
-                                    <Route path="/login" element={<LoginPage />} />
-                                    {/* PrivateRoute is a component that checks if the user is logged in */}
-                                    <Route path="/subjects" element={<PrivateRoute element={<AllSubjects />} />} />
-                                </Routes>
-                            </Suspense>
-                        </main>
-                    </div>
-                </Router>
-            </div>
-
-            <h4>Računarski Fakultet</h4>
-            <p>{currentDate}</p>
-        </>
-    );
-}
-
-export default App;
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.tsx'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-    throw new Error('Root element not found');
-}
-
-createRoot(rootElement).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
